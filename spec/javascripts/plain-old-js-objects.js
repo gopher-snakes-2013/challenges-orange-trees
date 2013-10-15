2405563f272b7d5f6127ad9c9510cbab76da97f1
bbeaird <apprentice@dbc18.local>
2013-10-15 10:59:13 -0700

    move js of first release to correct file

---
+++ spec/javascripts/plain-old-js-objects.js
@@ -0,0 +1,38 @@
+FRUIT_BEARING_AGE = 3;
+MAX_AGE = 50;
+
+function createTree() {
+  return {
+    age: 0,
+    height: 0,
+    orangeCount: 0,
+    isAlive: true,
+
+    grow: function() {
+      this.age += 1;
+      this.height += 10;
+
+      if(this.age >= FRUIT_BEARING_AGE){
+        this.orangeCount += Math.floor(Math.random()*10)+1;
+      }
+      if(this.age > MAX_AGE){
+        this.die();
+      }
+    },
+
+    die: function() {
+      this.isAlive = false;
+    },
+
+    dropOrange: function() {
+      return "";
+    }
+  };
+  
+}
+
+function createOrange() {
+  return {
+    diameter: Math.floor(Math.random()*10)+1
+  };
+}
