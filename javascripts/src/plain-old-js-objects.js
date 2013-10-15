584c2609e08b874c43e55edeaf85fddb990aa841
Apprentice <apprentice@dbc09.local>
2013-10-15 10:49:55 -0700

    passed all plain old js tests. not sure how to drop orange object though...

--- javascripts/src/plain-old-js-objects.js
+++ javascripts/src/plain-old-js-objects.js
@@ -1,1 +1,39 @@
 /* Write your code for the plain-old-js-objects-spec here */
+var FRUIT_BEARING_AGE = 5
+var MAX_AGE = 15
+
+var createTree = function(){
+  return {
+    age: 0,
+    height: 0,
+    orangeCount: 0,
+    isAlive: true,
+
+    grow: function(){
+      this.age += 1; 
+      this.height += 10;
+      this.orangeCount += Math.floor(Math.random()*this.age)
+      this.die();
+    },
+
+    die: function(){
+      if(this.age >= MAX_AGE){
+        this.isAlive = false;
+      }
+    },
+
+    dropOrange: function(){
+      return "this is a lie"
+    }
+  }  
+}
+
+var createOrange = function(){
+  return {
+    diameter: Math.floor((Math.random()+1)*5)
+  }
+}
+
+
+
+
---
+++ spec/javascripts/typed-objects-spec.js
@@ -0,0 +1,8 @@
+describe( "tree", function(){
+  
+  it "should exist", function(){
+    expect(tree = new Tree).toBeDefined();
+
+  });
+});
+
