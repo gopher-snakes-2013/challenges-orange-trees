c4b136147599ec86a5bddc51be3c6fbfbee46ca5
Laura Mead <lauramead@Lauras-MacBook-Air-2.local>
2013-10-15 12:21:55 -0700

    add release 1

--- javascripts/src/plain-old-js-objects.js
+++ javascripts/src/plain-old-js-objects.js
@@ -1,1 +1,26 @@
-/* Write your code for the plain-old-js-objects-spec here */
+function createTree() {
+  var tree = {
+    age: 0,
+    height: 0,
+    orangeCount: 0,
+    grow: function() { 
+    	this.age += 1, 
+    	this.height += 10;
+      this.orangeCount += 6},
+    dropOrange: function() {
+    	this.orangeCount -= 1;
+    	return 1;
+    },
+  };
+  return tree;
+}
+
+function createOrange() {
+	var orange = {
+		diameter: Math.random() * 3
+	};
+	return orange;
+}
+
+var FRUIT_BEARING_AGE = 7;
+var MAX_AGE = 100;
---
+++ javascripts/src/typed-objects.js
@@ -0,0 +1,7 @@
+var Tree = function() {
+	this.age = 0;
+};
+
+var tree = new Tree();
+
+Tree.prototype.age = 0;
