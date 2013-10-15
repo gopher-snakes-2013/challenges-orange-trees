08d8625e79ce8c79863c61a1ffc0d3bd8dfa49a0
bbeaird <bbeaird@gmail.com>
2013-10-15 10:14:53 -0700

    plain-js-objects passes 11 tests

--- javascripts/src/plain-old-js-objects.js
+++ javascripts/src/plain-old-js-objects.js
@@ -1,1 +1,33 @@
 /* Write your code for the plain-old-js-objects-spec here */
+function createTree() {
+  FRUIT_BEARING_AGE = 3;
+  MAX_AGE = 10;
+  return {
+    age: 0,
+    height: 0,
+    orangeCount: 0,
+    isAlive: true,
+    grow: function() {
+      this.age += 1;
+      this.height += 10;
+      if (this.age >= FRUIT_BEARING_AGE) {
+        this.orangeCount += Math.floor(Math.random()*10);
+      };
+      if (this.age >= MAX_AGE) {
+        this.isAlive = false;
+      };
+    },
+    dropOrange: function () {
+      this.orangeCount--;
+      return createOrange();
+    }
+  };
+};
+
+function createOrange() {
+  return {
+    color: "orange",
+    flavor: "yummy",
+    diameter: 20
+  };
+}
