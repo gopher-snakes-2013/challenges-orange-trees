6c4458d4c0404a21cd2f41011521da83522f92a9
Apprentice <apprentice@dbc17.local>
2013-10-15 10:51:03 -0700

    made some oranges the easy way

--- javascripts/src/plain-old-js-objects.js
+++ javascripts/src/plain-old-js-objects.js
@@ -1,1 +1,34 @@
 /* Write your code for the plain-old-js-objects-spec here */
+function createTree () {
+	var tree = {};
+	tree.age = 0;
+	tree.height = 0;
+	tree.orangeCount = 0;
+	tree.grow = function () {
+		this.age += 1;
+		this.height += 10;
+		this.dropOrange = function () {
+			return createOrange();
+		};
+		if (tree.age >= FRUIT_BEARING_AGE) {
+			tree.orangeCount += Math.floor((Math.random()*10)+1);
+		}; 
+		if (tree.age <= MAX_AGE){
+			tree.isAlive = true;
+		}
+		else
+		{
+			tree.isAlive = false;
+		}
+	};
+	return tree;
+}
+
+function createOrange(){
+	var orange = {
+		diameter: Math.floor((Math.random()*10)+1)};
+	return orange;
+}
+
+FRUIT_BEARING_AGE = 2;
+MAX_AGE = 20;
