aeb4afe4ef4e682700b9ee1b41d51989a42192dd
bbeaird <bbeaird@gmail.com>
2013-10-19 23:35:29 -0700

    R1 and R2 complete

--- javascripts/src/plain-old-js-objects.js
+++ javascripts/src/plain-old-js-objects.js
@@ -1,1 +1,36 @@
 /* Write your code for the plain-old-js-objects-spec here */
+// with JS objects, there is only one tree you can create. 
+
+FRUIT_BEARING_AGE = 4,
+MAX_AGE = 40
+
+function createTree(){
+  var tree = {
+              age: 0,
+              height: 0,
+              orangeCount: 0,
+              isAlive: true,
+              grow: function(){
+                this.age += 1,
+                this.height +=10,
+                this.orangeCount += Math.floor((Math.random()*10)+1)
+                if (this.age > MAX_AGE){
+                  this.die();
+                }
+              },
+              die: function(){
+                this.isAlive = false;
+              },
+
+              dropOrange: function(){
+                return this.orangeCount
+              }
+
+              
+            }
+  return tree
+}
+
+function createOrange(){
+  return orange = { diameter: Math.floor((Math.random()*10)+1)}
+}
---
+++ javascripts/src/typed-objects.js
@@ -0,0 +1,48 @@
+// with constructors and prototypes, you have a general casting mold 
+//which can be adjusted. For example, casting it with a specific age or
+// height. You can't do that with just plain js objects.  
+
+FRUIT_BEARING_AGE = 4;
+MAX_AGE = 40;
+// same as 'function Tree (age, height){'
+var Tree = function (age, height){
+  this.age = age; 
+  this.height = height;
+  this.orangeCount = 0;
+  // setting isAlive to false because I can't get the die if loop 
+  // to work.  
+  this.isAlive = false
+
+};
+
+
+// Rather than putting the grow function in the Tree function, you 
+// can set it as a prototype function. This is faster as this means
+// the grow function doesn't have to be re-created every time a new
+// // tree is created. 'Grow' is only summoned when you need it to be
+// // summoned.
+
+Tree.prototype.grow = function(){ 
+  this.age += 1;
+  this.height += 10;
+  this.orangeCount += Math.floor(Math.random()*10+1 )
+
+  // why can't I get this if loop to work?
+  // if (this.age >= FRUIT_BEARING_AGE) {
+  //     this.orangeCount += Math.floor(Math.random()*10+1;
+  //   )}
+};
+
+
+Tree.prototype.die = function(){
+  // OMG WHAT IS WRONG WITH MY IF LOOPS??!?!?
+ if (this.age > MAX_AGE) {
+    this.isAlive = false;
+  }
+  return this.isAlive
+};
+
+var Orange = function(){
+  this.diameter = 5
+}
+
